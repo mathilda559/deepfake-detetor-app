b799d8c9ee3abaa8370e0909b7824aa62e87ee8c
mathilda559 <wulfma@katharineum.de>
2026-05-28 12:44:24 +0200

    einsetzen face utils

--- face_utils.py
+++ face_utils.py
@@ -1,1 +1,24 @@
+import cv2
+import numpy as np
 
+face_net = cv2.dnn.readNetFromCaffe(
+    "https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt",
+    "https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel"
+)
+
+def detect_face(frame):
+    h, w = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 117, 123))
+
+    face_net.setInput(blob)
+    detections = face_net.forward()
+
+    faces = []
+
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            faces.append(box.astype(int))
+
+    return faces
